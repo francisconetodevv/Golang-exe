58177b4dec7b35a17c8872f911baa361b7b2926b
Francisco <franciscoocontatoo@gmail.com>
2025-04-12 12:36:36 -0300

    Enviando atualizações sobre o exercício - Tratativas de erros no golang

--- main.go
+++ main.go
@@ -9,15 +9,19 @@ import (
 func main() {
 	fmt.Println("Testando...")
 
-	var items []model.ItemCompras
-	items = append(items, model.ItemCompras{NomeItem: "Arroz"})
-	items = append(items, model.ItemCompras{NomeItem: "Feijao"})
+	/*Inicializando o slice*/
+	var nomeItens []string
+	nomeItens = append(nomeItens, "Arroz")
+	nomeItens = append(nomeItens, "Feijao")
+	nomeItens = append(nomeItens, "Carne")
+	nomeItens = append(nomeItens, "Macarrão")
 
-	compras := model.Compras{
-		NomeMercado: "Americanas",
-		Horario:     time.Now(),
-		Itens:       items,
+	compras, err := model.NewCompra("Mercadinho Jesus", time.Now(), nomeItens)
+
+	if err != nil {
+		fmt.Println(err.Error())
+	} else {
+		fmt.Println(compras)
 	}
 
-	fmt.Println(compras)
 }
--- model/compras.go
+++ model/compras.go
@@ -2,7 +2,10 @@
 
 package model
 
-import "time"
+import (
+	"errors"
+	"time"
+)
 
 type Compras struct {
 	NomeMercado string
@@ -17,6 +20,29 @@ type ItemCompras struct {
 	NomeItem string
 }
 
-func (c *Compras) CalcularHorarioCompras() {
-	c.Horario = time.Now()
+/* Método para realizar compra */
+/* Passamos os parâmetros da compra que são os itens, o retorno será a compra e um erro, caso ocorra.*/
+/* É preciso retornar o erro também, pois podemos tratar caso o usuário não passar nenhum valor */
+func NewCompra(mercado string, data time.Time, nomeItens []string) (*Compras, error) {
+
+	/*Validando se as informações repassadas para função se são nulas ou não*/
+	if mercado == "" {
+		return nil, errors.New("Mercado é obrigatório")
+	}
+
+	if len(nomeItens) == 0 {
+		return nil, errors.New("Itens são obrigatórios")
+	}
+
+	var itens []ItemCompras
+
+	for _, nome := range nomeItens {
+		itens = append(itens, ItemCompras{NomeItem: nome})
+	}
+
+	return &Compras{
+		NomeMercado: mercado,
+		Horario:     time.Now(),
+		Itens:       itens,
+	}, nil
 }
